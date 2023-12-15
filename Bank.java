b7cea7e1e0514c3b0e876812ad94d643aaa58eef
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2023-12-15 22:04:48 +0200

    printing

--- Bank.java
+++ Bank.java
@@ -10,6 +10,7 @@ public class Bank {
 	}
 	public void addCustomer(Customer cutomer) {
 		customers.add(cutomer);
+		System.out.println("Cusomer " + cutomer.getName() + "Added");
 	}
 	public void removeCustomer(Customer customer) {
 		customers.remove(customer);
@@ -17,11 +18,13 @@ public class Bank {
 	public void openAccount(Customer customer, double balnce) {
 		Account account = new Account(customer, balnce);
 		accounts.add(account);
+		System.out.println("account for " + cutomer.getName() + "created");
+		System.out.println("Balance: " + account.getBalance());
 	}
 	public void deposite(Account account, double amount) {
 		
 		account.setBalance(account.getBalance() + amount);
-		 
+		
 	}
 	public void withdraw(Account account, double amount) {
 		account.setBalance(account.getBalance() - amount);
