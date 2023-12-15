5945a3fd81259188ac2206a5af502f7e5247be69
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2023-12-15 22:00:50 +0200

    main implementation

--- Main.java
+++ Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Bank bank = new Bank();
+		Customer mahmoud = new Customer("Mahmoud", 19);
+		
+		bank.addCustomer(mahmoud);
+		
+		bank.openAccount(mahmoud, 100.0);
 	}
 
 }
