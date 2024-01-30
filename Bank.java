cf15a23d97031593a0da98c74c4abc551929ebf0
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-01-30 12:02:07 +0200

    fixing deposit method

--- Bank.java
+++ Bank.java
@@ -8,13 +8,16 @@ public class Bank {
 		this.customers = new ArrayList<>();
 		this.accounts = new ArrayList<>();
 	}
+	
 	public void addCustomer(Customer cutomer) {
 		customers.add(cutomer);
 		System.out.println("Customer " + cutomer.getName() + " Added.");
 	}
+	
 	public void removeCustomer(Customer customer) {
 		customers.remove(customer);
 	}
+	
 	public void openAccount(Customer customer, double balance) {
 		if (balance > 0) {Account account = new Account(customer, balance);
 		accounts.add(account);
@@ -23,14 +26,16 @@ public class Bank {
 		else {System.out.println("Cannot add a zero or negative value.");}
 		
 	}
+	
 	public void deposite(Account account, double amount) {
-		
-		account.setBalance(account.getBalance() + amount);
-		
+		if (amount > 0) {account.setBalance(account.getBalance() + amount);}
+		else {System.out.println("Cannot add zero or negative value.");}
 	}
+	
 	public void withdraw(Account account, double amount) {
 		account.setBalance(account.getBalance() - amount);
 	}
+	
 	public void transfer(Account account1, Account account2, double amount) {
 		account2.setBalance(account2.getBalance() + amount);
 		account1.setBalance(account1.getBalance() - amount);
