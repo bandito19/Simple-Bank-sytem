28ad916232fc2f6cb6f2e693490aae71d0c515c5
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-01-30 12:00:09 +0200

    fixing open account method

--- Bank.java
+++ Bank.java
@@ -15,11 +15,13 @@ public class Bank {
 	public void removeCustomer(Customer customer) {
 		customers.remove(customer);
 	}
-	public void openAccount(Customer customer, double balnce) {
-		Account account = new Account(customer, balnce);
+	public void openAccount(Customer customer, double balance) {
+		if (balance > 0) {Account account = new Account(customer, balance);
 		accounts.add(account);
 		System.out.println("account for " + customer.getName() + " created");
-		System.out.println("Balance: " + account.getBalance());
+		System.out.println("Balance: " + account.getBalance());}
+		else {System.out.println("Cannot add a zero or negative value.");}
+		
 	}
 	public void deposite(Account account, double amount) {
 		
