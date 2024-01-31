419c736ac93b8d37a94bfb5a571e724fa68d1d6b
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-01-31 11:47:18 +0200

    creating two accounts

--- Main.java
+++ Main.java
@@ -3,11 +3,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bank bank = new Bank();
-		Customer mahmoud = new Customer("Mahmoud");
 		
-		bank.addCustomer(mahmoud);
+		//first customer
+		Customer c1 = new Customer("C1");
+		bank.addCustomer(c1);
+		bank.openAccount(c1, 100000.0);
+		System.out.println("account id: " + c1.getId());
+		System.out.println();
 		
-		bank.openAccount(mahmoud, 100.0);
+		//second customer
+		Customer c2 = new Customer("C2");
+		bank.addCustomer(c2);
+		bank.openAccount(c2, 1387900.43);
+		System.out.println("account id: " + c2.getId());
+		System.out.println();
 	}
 
 }
